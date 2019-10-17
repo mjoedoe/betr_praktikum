cc96e96b46005b39c793560793f10b7d554e82ff
Johannes Reusch <joe.reusch92@gmail.com>
2019-10-17 10:20:11 +0200

    lad mal

---
+++ Reusch_Johannes_87771_Aufgabe_1_Programmcode.py
@@ -0,0 +1,62 @@
+import graphviz as gr 
+
+
+class ANDNODE:
+    def __init__(self, name):
+        self.name = name
+        self.nodes = []
+    def add(self, node):
+        self.nodes.append(node)
+        return
+    #???????????????????????????????????
+#    def topdown(self, mat):
+ #       return mat
+   
+ 
+ 
+class ORNODE:
+    def __init__(self, name):
+        self.name = name
+        self.nodes = []
+    def add(self, node):
+        self.nodes.append(node)
+        return
+    #????????????????????????????
+  #  def topdown(self, mat):
+   #     return mat
+   
+class EVENT: # EVENT = Eingang
+    def __init__(self, name):
+        self.name = name
+        return
+    #??????
+   # def topdown(self, mat):
+    #    return mat
+ 
+def  maptree (dot, top):
+    dot.node(top.name, top.name)
+    if top.nodes :    
+        for a in top.nodes :
+            dot.edge(top.name + (top.nodes[a]).name)
+            maptree(dot,top.nodes[a])
+    
+
+
+
+
+def main():
+    TOP = ANDNODE('TOP')
+    A = ORNODE('A')
+    B = ORNODE('B')
+    D = ANDNODE('D')
+    TOP.add(A)
+    TOP.add(B)
+    B.add(D)
+    A.add("input 1")
+    print(A.name)
+    dot = gr.Digraph('Baum')
+    maptree(dot, TOP)
+    print(dot.source)
+    dot.render('output')
+if __name__ == "__main__":
+    main()
