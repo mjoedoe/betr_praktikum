673b50e893f8aa4c6f50f33121065809e7605720
Johannes Reusch <joe.reusch92@gmail.com>
2019-10-18 23:06:07 +0200

    version vom laptop

--- Reusch_Johannes_87771_Aufgabe_1_Programmcode.py
+++ Reusch_Johannes_87771_Aufgabe_1_Programmcode.py
@@ -9,8 +9,8 @@ class ANDNODE:
         self.nodes.append(node)
         return
     #???????????????????????????????????
-#    def topdown(self, mat):
- #       return mat
+    def topdown(self, mat):
+       return mat
    
  
  
@@ -33,13 +33,17 @@ class EVENT: # EVENT = Eingang
    # def topdown(self, mat):
     #    return mat
  # alskdjajskld
+'''
 def  maptree (dot, top):
     dot.node(top.name, top.name)
     if top.nodes :    
         for a in top.nodes :
             dot.edge(top.name + (top.nodes[a]).name)
             maptree(dot,top.nodes[a])
-    
+    print(dot.source)
+    dot.render('output')
+'''
+
 
 
 
@@ -54,9 +58,10 @@ def main():
     B.add(D)
     A.add("input 1")
     print(A.name)
-    dot = gr.Digraph('Baum')
-    maptree(dot, TOP)
-    print(dot.source)
-    dot.render('output')
+    mat = [[TOP]]
+    print(mat)
+  #  dot = gr.Digraph('Baum')
+ #   maptree(dot, TOP)
+    
 if __name__ == "__main__":
     main()
